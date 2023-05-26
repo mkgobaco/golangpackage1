456bb53833883ffb245a40e2c0111b20ce4bdc8d
Michael Gobaco <michael.gobaco@pluto.tv>
2023-05-26 15:07:16 -0700

    update file1

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,8 @@
 package main
 
+import "github.com/mkgobaco/golangpackage2/pkg/package2"
+
 func main() {
 	println("Hello, World!")
+	println(package2.PackageName())
 }
--- pkg/package1/file1.go
+++ pkg/package1/file1.go
@@ -1,5 +1,5 @@
 package package1
 
 func PackageName() string {
-	return "package1"
+	return "package1 v0.0.1"
 }
